d08941c9e034b691a0804db6da5c4f7c6caa8bd0
MedveShyt <165942713+MedveShyt@users.noreply.github.com>
2024-04-04 17:47:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] arg) {
+//        int a=55;
+//        int b=20;
+//        int c= a/b;
+//        System.out.println(c);
+//        int d=a%b;
+//        System.out.println(d);
+
+//        int a=32;
+//        int b=a/10;
+//        int c=a%10;
+//        System.out.println(b+c);
+
+        double a=2.1;
+        int b= (int)(a/1);
+        System.out.println(b);
+
+
+
+    }
+
+}
